a95f6f23acb453e41d2bac3184bc0a904691025a
denios <76059129+deniosm@users.noreply.github.com>
2023-03-10 08:25:03 +0100

    Initial
    *

---
+++ debian/mate-hud/usr/lib/python3/dist-packages/setup.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2016 by Martin Wimpress <code@flexion.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+
+import os
+import sys
+
+from glob import glob
+from setuptools import setup
+
+import DistUtilsExtra.command.build_extra
+import DistUtilsExtra.command.build_i18n
+import DistUtilsExtra.command.clean_i18n
+
+__VERSION__ = '22.10.4+denios1'
+
+def datafilelist(installbase, sourcebase):
+    datafileList = []
+    for root, subFolders, files in os.walk(sourcebase):
+        fileList = []
+        for f in files:
+            fileList.append(os.path.join(root, f))
+        datafileList.append((root.replace(sourcebase, installbase), fileList))
+    return datafileList
+
+data_files = [
+    ('{prefix}/lib/mate-hud/'.format(prefix=sys.prefix), ['usr/lib/mate-hud/mate-hud']),
+    ('{prefix}/lib/mate-hud/'.format(prefix=sys.prefix), ['usr/lib/mate-hud/common.py']),
+    ('{prefix}/lib/mate-hud/'.format(prefix=sys.prefix), ['usr/lib/mate-hud/hud-settings.py']),
+    ('{prefix}/lib/mate-hud/'.format(prefix=sys.prefix), ['usr/lib/mate-hud/i18n.py']),
+    ('{prefix}/lib/mate-hud/'.format(prefix=sys.prefix), ['usr/lib/mate-hud/getkey_dialog.py']),
+    ('{prefix}/lib/mate-hud/'.format(prefix=sys.prefix), ['usr/lib/mate-hud/getkey_dialog.ui']),
+    ('{prefix}/share/applications/'.format(prefix=sys.prefix), ['usr/share/applications/hud-settings.desktop']),
+    ('{prefix}/share/mate/autostart/'.format(prefix=sys.prefix), ['usr/share/mate/autostart/mate-hud.desktop']),
+    ('{prefix}/share/glib-2.0/schemas/'.format(prefix=sys.prefix), ['usr/share/glib-2.0/schemas/org.mate.hud.gschema.xml']),
+    ('{prefix}/share/pixmaps/'.format(prefix=sys.prefix), ['usr/share/pixmaps/mate-hud.svg']),
+    ('{prefix}/share/icons/hicolor/16x16/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/16x16/apps/mate-hud.png']),
+    ('{prefix}/share/icons/hicolor/22x22/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/22x22/apps/mate-hud.png']),
+    ('{prefix}/share/icons/hicolor/24x24/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/24x24/apps/mate-hud.png']),
+    ('{prefix}/share/icons/hicolor/32x32/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/32x32/apps/mate-hud.png']),
+    ('{prefix}/share/icons/hicolor/48x48/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/48x48/apps/mate-hud.png']),
+    ('{prefix}/share/icons/hicolor/64x64/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/64x64/apps/mate-hud.png']),
+    ('{prefix}/share/icons/hicolor/128x128/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/128x128/apps/mate-hud.png']),
+    ('{prefix}/share/icons/hicolor/256x256/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/256x256/apps/mate-hud.png']),
+    ('{prefix}/share/icons/hicolor/512x512/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/512x512/apps/mate-hud.png']),
+    ('{prefix}/share/icons/hicolor/16x16@2/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/16x16@2/apps/mate-hud.png']),
+    ('{prefix}/share/icons/hicolor/22x22@2/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/22x22@2/apps/mate-hud.png']),
+    ('{prefix}/share/icons/hicolor/24x24@2/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/24x24@2/apps/mate-hud.png']),
+    ('{prefix}/share/icons/hicolor/32x32@2/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/32x32@2/apps/mate-hud.png']),
+    ('{prefix}/share/icons/hicolor/48x48@2/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/48x48@2/apps/mate-hud.png']),
+    ('{prefix}/share/icons/hicolor/64x64@2/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/64x64@2/apps/mate-hud.png']),
+    ('{prefix}/share/icons/hicolor/128x128@2/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/128x128@2/apps/mate-hud.png']),
+    ('{prefix}/share/icons/hicolor/scalable/apps'.format(prefix=sys.prefix), ['usr/share/icons/hicolor/scalable/apps/mate-hud.svg']),
+    ('{prefix}/share/rofi/themes/'.format(prefix=sys.prefix), ['usr/share/rofi/themes/mate-hud.rasi']),
+    ('{prefix}/share/rofi/themes/'.format(prefix=sys.prefix), ['usr/share/rofi/themes/mate-hud-hidpi.rasi']),
+    ('{prefix}/share/rofi/themes/'.format(prefix=sys.prefix), ['usr/share/rofi/themes/mate-hud-rounded.rasi']),
+    ('{prefix}/share/rofi/themes/'.format(prefix=sys.prefix), ['usr/share/rofi/themes/mate-hud-rounded-hidpi.rasi']),
+]
+
+cmdclass = {
+        "build" : DistUtilsExtra.command.build_extra.build_extra,
+        "clean": DistUtilsExtra.command.clean_i18n.clean_i18n,
+}
+
+setup(
+    name = "mate-hud",
+    version = __VERSION__,
+    description = "Run menubar commands, much like the Unity 7 HUD",
+    license = 'GPLv2+',
+    author = 'Martin Wimpress',
+    url = 'https://github.com/deniosm/mate-hud/',
+    package_dir = {'': '.'},
+    data_files = data_files,
+    install_requires = ['setuptools'],
+    #scripts = [],
+    cmdclass = cmdclass,
+)
